7d7fe0633c4d43975dd9839dda7e41f6de6a5c57
andreadldev <andrea.delisi2@gmail.com>
2023-02-02 10:53:36 +0100

    Aggiungi path immagini

--- src/components/__projectCard.vue
+++ src/components/__projectCard.vue
@@ -20,7 +20,7 @@
     <div class="d-flex flex-wrap p-5">
         <div class="card mx-1 mb-5" style="width: 18rem;" v-for="project in this.projects">
             <h4>{{ project.id }}</h4>
-            <img src=https://via.placeholder.com/150 class="card-img-top" alt="...">
+            <img v-if="project.image_url" :src="project.image_url" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ project.name }}</h5>
                 <p class="card-text">{{ project.description }}</p>
